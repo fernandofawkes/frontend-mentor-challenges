cb89bdda5ba7618019392d27246137b4391d39f5
fernandofawkes <contato.fernandom@gmail.com>
2020-05-22 18:04:48 -0300

    feat: setup

---
+++ SocialMediaDashboard/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = '';
+
+ReactDOM.render(<div>{title}</div>, document.getElementById('app-root'));
+
+module.hot.accept();
